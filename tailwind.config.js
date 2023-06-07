abc9342e35d05133d1e1f9e042e9ff8366034183
chinewueze <chiderauba83@gmail.com>
2023-06-07 08:03:06 +0100

    modified breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     screens: {
-      sm: "350px",
+      sm: "380px",
       md: "768px",
       lg: "976px",
       xl: "1440px"
