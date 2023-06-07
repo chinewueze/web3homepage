e74b89d53f3e9720df1bd10f27566c61925d70f8
chinewueze <chiderauba83@gmail.com>
2023-06-07 07:39:18 +0100

    modified viewport width

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     screens: {
-      sm: "480px",
+      sm: "350px",
       md: "768px",
       lg: "976px",
       xl: "1440px"
