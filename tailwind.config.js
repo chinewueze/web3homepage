e13461300b69aa0916ab1cd5fae08af3dd3a4b9c
chinewueze <chiderauba83@gmail.com>
2023-06-07 08:07:54 +0100

    reviewed screen width

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     screens: {
-      sm: "380px",
+      sm: "330px",
       md: "768px",
       lg: "976px",
       xl: "1440px"
